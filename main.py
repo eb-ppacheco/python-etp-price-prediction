ba45f061538b71a61ba5ced98c9b9244df775e18
Paola Pacheco <ppacheco@eventbrite.com>
2019-05-23 15:45:02 -0300

    add comments TO DO

--- main.py
+++ main.py
@@ -7,18 +7,16 @@ def get_prediction(country_code, event_category):
                            usecols=['currency', 'id', 'country', 'category_id',
                                     'price_ticket_amount', 'purchased_at', 'quantity_sold'])
     # filter by category and country
+    # TO DO -create and  validate an user input
     subset_df_filter_one = df.query('country == "US" & category_id == "101.0"')
 
     # filter by date
-    # convert_date = df['purchased_at'] = pd.to_datetime(df['purchased_at'])
-    # date_before = '26-01-2019'
-    # mask = (df['purchased_at'] > date_before)
-    # filter_by_date = df.loc[mask]
-
+    # TO DO - create a variable to calculate timezone.now - 30 days
     subset_df_filter_date = subset_df_filter_one.loc[(subset_df_filter_one['purchased_at'] < '26-01-2019')]
 
     # calculate ticket price per unit
     price_ticket_per_unit = subset_df_filter_date['price_ticket_amount']/subset_df_filter_date['quantity_sold']
+    # TO DO - validate if currency it's the same, it means that you can't divide USD/BRL
     mean_ticket = price_ticket_per_unit.mean()
     std_ticket = price_ticket_per_unit.std()
 
